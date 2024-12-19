5c5e9530aa05cf366a65abce45f2b689413617b1
Akira Hayashi <rindrics@gmail.com>
2024-12-20 02:51:40 +0900

    Correct parser

--- lib/parse.py
+++ lib/parse.py
@@ -1,5 +1,8 @@
 import re
 
+PATTERN_CRUISE = r"^[A-Z]{2}\d{4}"  # Example: TK8707
+PATTERN_STATION = r"A[0-2][0-9]"    # Example: A01
+
 def is_metadata(line):
     """
     Determines if a line is a metadata line.
@@ -16,31 +19,46 @@ def is_metadata(line):
         >>> is_metadata("TK8707         A01                                                                        .00  11.8280  32.7850  11.8252")
         False
     """
-    pattern_cruise = r"^[A-Z]{2}\d{4}"  # Example: TK8707
-    pattern_station = r"A[0-2][0-9]"    # Example: A01
     pattern_year = r"\d{4}"             # Example: 1987
     pattern_month = r"[0-1]?[0-9]"      # Example: 7 or 07
-    pattern_day = r"[0-3]?[0-9]"        # Example: 18
+    pattern_day = r"[0-3]?[0-9]"        # Ewmxample: 18
     pattern_hour = r"[0-2]?[0-9]"       # Example: 17
     pattern_minute = r"[0-6]?[0-9]"     # Example: 40
-    pattern_lat = r"[0-1]?[0-8]?[0-9]\s+[0-6]?[0-9]\.[0-9]\s+[NS]"  # Example: 42 50.0 N
-    pattern_lon = r"[0-1]?[0-8]?[0-9]\s+[0-6]?[0-9]\.[0-9]\s+[EW]"  # Example: 144 49.9 E
+    pattern_lat = r"[0-1]?[0-8]?[0-9]\s+[0-6]?[0-9]\.[0-9]+\s*[NS]?"  # Example: 42 50.0 N
+    pattern_lon = r"[0-1]?[0-8]?[0-9]\s+[0-6]?[0-9]\.[0-9]+\s*[EW]?"  # Example: 144 49.9 E
 
+    pattern = f"{PATTERN_CRUISE}\\s+{PATTERN_STATION}\\s+{PATTERN_STATION}\\s+{pattern_year}\\s+{pattern_month}\\s+{pattern_day}\\s+{pattern_hour}\\s+{pattern_minute}\\s+{pattern_lat}\\s+{pattern_lon}"
+
+    match = re.search(pattern, line)
+    return match is not None
+
+
+def validate_record(record):
+    """
+    Raise error if given record is in unexpected format.
+
+    Args:
+        record (str): record to validate
+
+    Returns:
+        None
+
+    Examples:
+        >>> validate_record("TK8707         A01        A01    1987  7 18  17 40  42 50.0 N 144 49.9 E") is None
+        True
+
+        >>> validate_record("TK8707         A09        A09    1987  7 20   1 15  41  0.0145  44  0.0") is None
+        True
+    """
+    pattern_pressure = r"[0-9]*\.[0-9]{2}"
     patterns = [
-        pattern_cruise,
-        pattern_station,
-        pattern_station,
-        pattern_year,
-        pattern_month,
-        pattern_day,
-        pattern_hour,
-        pattern_minute,
-        pattern_lat,
-        pattern_lon
+        PATTERN_CRUISE,
+        PATTERN_STATION,
+        pattern_pressure,
     ]
-    # Combine all patterns with spaces in between
-    return all(re.search(pattern, line) for pattern in patterns)
 
+    if not all(re.search(pattern, record) for pattern in patterns):
+        raise ValueError("given record is unexpected format")
 
 def metadata(line):
     """
@@ -55,6 +73,7 @@ def metadata(line):
     Examples:
         >>> metadata("TK8707         A01        A01    1987  7 18  17 40  42 50.0 N 144 49.9 E")
         'TK8707         A01        A01    1987  7 18  17 40  42 50.0 N 144 49.9 E'
+
         >>> metadata("TK8707         A01                                                                        .00  11.8280  32.7850  11.8252")
         ''
     """
@@ -74,6 +93,7 @@ def record(line):
     Examples:
         >>> record("TK8707         A01        A01    1987  7 18  17 40  42 50.0 N 144 49.9 E")
         ''
+
         >>> record("TK8707         A01                                                                        .00  11.8280  32.7850  11.8252")
         'TK8707         A01                                                                        .00  11.8280  32.7850  11.8252'
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dlt.sources.helpers import requests
+from lib.parse import metadata, record
 
-def main(page_url):
+def main(page_url, file_metadata, file_record):
     base_url = "https://gist.githubusercontent.com/Rindrics/"
 
     response = requests.get(
@@ -9,8 +10,25 @@ def main(page_url):
     )
     print(response.text)
 
-    return response
+    lines = response.text.splitlines()
+
+    with open(file_metadata, 'a') as meta_file, open(file_record, 'a') as rec_file:
+        for line in lines:
+
+            # skip if blank line
+            if not line.strip():
+                continue
+
+            result_metadata = metadata(line)
+            if not result_metadata == "":
+                meta_file.write(f"{result_metadata}\n")
+
+            result_record = record(line)
+            if not result_record == "":
+                rec_file.write(f"{result_record}\n")
+
+    return None
 
 if __name__ == "__main__":
     page_url = "689288da5d327f633a6d640ddec27009/raw/97de6556d3798d08f41ae09207077f594ec6a665/gistfile1.txt"
-    main(page_url)
+    main(page_url, "metadata_test.txt", "record_test.txt")
