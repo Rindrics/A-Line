9fec59c1ae0edd446eb8e8983aaa000d454ecd5d
Akira Hayashi <rindrics@gmail.com>
2024-12-20 01:09:44 +0900

    Simplify code

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from dlt.sources.helpers.rest_client import RESTClient
-import requests
+from dlt.sources.helpers import requests
 
-def main(session):
-    client = RESTClient(
-        base_url="https://gist.githubusercontent.com/Rindrics/",
-        headers={"User-Agent": "MyApp/1.0"},
-        data_selector="data",
-        session=session,
+def main(page_url):
+    base_url = "https://gist.githubusercontent.com/Rindrics/"
+
+    response = requests.get(
+        base_url + page_url,
+        headers={},
     )
-    response = client.get("689288da5d327f633a6d640ddec27009/raw/97de6556d3798d08f41ae09207077f594ec6a665/gistfile1.txt")
     print(response.content)
+
     return response
 
 if __name__ == "__main__":
-    session = requests.Session()
-    main(session)
+    page_url = "689288da5d327f633a6d640ddec27009/raw/97de6556d3798d08f41ae09207077f594ec6a665/gistfile1.txt"
+    main(page_url)
