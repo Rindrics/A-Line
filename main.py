a0ccb56ed91bec9ab001b6045b362a9e7926c7cd
Akira Hayashi <rindrics@gmail.com>
2024-12-22 16:48:02 +0900

    Load `dim_observations`

--- main.py
+++ main.py
@@ -10,15 +10,60 @@ def a_line_source(page_url: str):
     """CTD data of A-Line"""
 
     @dlt.resource(
-        name="metadata",
+        name="dim_observations",
         write_disposition="replace",
-        primary_key=["cruise", "station"]
+        primary_key=["observation_id"]
     )
-    def get_metadata() -> Iterator[Dict[str, Any]]:
+    def get_observations() -> Iterator[Dict[str, Any]]:
+        metadata_df = get_metadata()
+
+        metadata_df['observation_id'] = metadata_df['cruise'] + '_' + metadata_df['station']
+
+        observations_df = metadata_df.groupby('observation_id').agg({
+            'cruise': 'first',
+            'station': 'first',
+            'year': 'first',
+            'month': 'first',
+            'day': 'first',
+            'hour': 'first',
+            'minute': 'first',
+            'lat_deg': 'first',
+            'lat_min': 'first',
+            'lat_dir': 'first',
+            'lon_deg': 'first',
+            'lon_min': 'first',
+            'lon_dir': 'first'
+        }).reset_index()
+
+        observations_df['observed_at'] = pd.to_datetime(
+            observations_df[['year', 'month', 'day', 'hour', 'minute']]
+        )
+
+        observations_df['latitude'] = observations_df.apply(
+            lambda x: x['lat_deg'] + x['lat_min']/60 * (1 if x['lat_dir']=='N' else -1),
+            axis=1
+        )
+        observations_df['longitude'] = observations_df.apply(
+            lambda x: x['lon_deg'] + x['lon_min']/60 * (1 if x['lon_dir']=='E' else -1),
+            axis=1
+        )
+
+        observations_df = observations_df[[
+            'observation_id',
+            'station',
+            'observed_at',
+            'latitude', 'longitude'
+        ]]
+        observations_df = observations_df.rename(columns={'station': 'station_id'})
+
+        for obs in observations_df.to_dict('records'):
+            yield obs
+
+    def get_metadata() -> pd.DataFrame:
         metadata_str = extract_data(page_url, data_type="metadata")
         metadata_io = io.StringIO(metadata_str)
 
-        df = pd.read_csv(
+        return pd.read_csv(
             metadata_io,
             sep='\s+',
             header=None,
@@ -28,9 +73,6 @@ def a_line_source(page_url: str):
                 'lon_deg', 'lon_min', 'lon_dir'
             ]
         )
-
-        for record in df.to_dict('records'):
-            yield record
 
     @dlt.resource(
         name="records",
